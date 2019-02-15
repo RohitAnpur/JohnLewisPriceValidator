c2fdb02b038245d7ea472ff4d3cf44753bc331a1
Rohit <rohit.anpur@gmail.com>
2019-02-15 22:13:30 +0000

    Initial Chnages

---
+++ pricevalidator/src/main/java/com/jl/org/pricevalidator/PricevalidatorApplication.java
@@ -0,0 +1,14 @@
+package com.jl.org.pricevalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricevalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricevalidatorApplication.class, args);
+	}
+
+}
+
